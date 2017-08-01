0369eeb36f05cd99eb33723a8dd2ff1b92efc15f
metalcorex <colbyschneider@gmail.com>
2017-08-01 16:49:39 -0400

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,1 @@
+Concept for a new mobile menu
---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+$(function() {
+
+    $('a.menu-toggle').click(function() {
+        $('.nav-wrapper-wrapper').slideToggle(250);
+        return false;
+    });
+
+});
